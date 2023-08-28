79b19d4ed567275f64ac21cd102e80aa76368436
matrixtechlabs <matrixtechlabs@gmail.com>
2023-08-28 23:34:52 +0530

    css for skills

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import { StickyNavbar,Home,Work,About,Contact } from './Components/index'
+// import { StickyNavbar,Home,Work,About,Contact } from './Components'
+import Home from './Components/Home/Home'
+import StickyNavbar from './Components/StickyNavbar/StickyNavbar'
+import About from './Components/About/About'
+import Work from './Components/Work/Work'
+import Contact from './Components/Contact/Contact'
+
 
 function App() {
   return (
--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -2,13 +2,14 @@ function Work(){
     return(
         <div className="bg-gray-800 text-white " >
         <div className="flex justify-center text-7xl">Our Skills</div>
-            <div className="flex" style={{ flexWrap: "wrap" }}>
-                <div className="flex justify-center w-full h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]" >NodeJs</div>
-                <div className="flex justify-center w-full h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]" >React</div>
-                <div className="flex justify-center w-full h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]" >Js</div>
-                <div className="flex justify-center w-full h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]" >Flutter</div>
-                <div className="flex justify-center w-full h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]" >HTML & CSS</div>
-            </div>
+        <div className="flex flex-wrap justify-around">
+            <div className="flex justify-center w-[100px] h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]">NodeJs</div>
+            <div className="flex justify-center w-[100px] h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]">React</div>
+            <div className="flex justify-center w-[100px] h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]">Js</div>
+            <div className="flex justify-center w-[100px] h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]">Flutter</div>
+            <div className="flex justify-center w-[100px] h-[100px] p-3 bg-gray-600 m-3 border border-gray-400 rounded-[25px]">HTML & CSS</div>
+        </div>
+
         </div>
     );
 }
